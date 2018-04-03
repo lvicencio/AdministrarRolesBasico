b9e1dd69d92644796fe3355f20aa5f6bd53e1fce
L Vicencio <lvicencio@gmail.com>
2018-04-03 00:19:53 -0300

    Creacion de rol provider

--- AdministrarRolesBasico/Controllers/HomeController.cs
+++ AdministrarRolesBasico/Controllers/HomeController.cs
@@ -1,16 +1,61 @@
-﻿using System;
+﻿using AdministrarRolesBasico.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace AdministrarRolesBasico.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuarios model, string returnUrl)
+        {
+
+            netRolEntities db = new netRolEntities();
+
+            var dataItem = db.Usuarios.Where(x => x.Username == model.Username && x.Password == model.Password).FirstOrDefault();
+
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.FullName, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                    && !returnUrl.StartsWith("//")
+                    && !returnUrl.StartsWith("/\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Usuario/Contraseña no es Valido");
+                return View();
+            }
+             
+        }
+
+        [Authorize]
+        public  ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
 
         public ActionResult About()
